433d888e15c43453816cfb0b9242ebee14e76eba
Ritik Drona <ritik1218@gmail.com>
2022-08-21 20:24:49 +0530

    Modification in heroku.yml

--- apps/api/src/database.ts
+++ apps/api/src/database.ts
@@ -4,9 +4,9 @@ import mongoose from 'mongoose'
 dotenv.config()
 
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}` +
-    `@${process.env.DB_HOST}/${process.env.DB_NAME}`,
-  () => console.log('DB Connected Successfully !')
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}` +
+        `@${process.env.DB_HOST}/${process.env.DB_NAME}`,
+    () => console.log('DB Connected Successfully !')
 )
 
 export default mongoose
--- apps/api/src/router.ts
+++ apps/api/src/router.ts
@@ -3,7 +3,7 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.send({ message: 'Hello World!' })
+    res.send({ message: 'Hello World!' })
 })
 
 export default router
